165a64b970c128905b4704ed0e421f37f4772403
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2022-12-01 20:17:18 +0300

    main added

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
+import warnings
+
+import numpy as np
 import pytorch_lightning as pl
 import rul_datasets
-from rul_datasets import CmapssReader
 
-from models import models
+from models.models import *
 
-# import rul_estimator # for fd in [1, 2, 3, 4]:
+pl.seed_everything(42)
+warnings.filterwarnings("ignore", ".*does not have many workers.*")
 
 if __name__ == "__main__":
-    for fd in [1]:
-        cmapss: CmapssReader = rul_datasets.CmapssReader(fd)
-        dm = rul_datasets.RulDataModule(cmapss, batch_size=32)
-
-        my_rul_estimator = rul_estimator.LSTM(16, 96, 4)
-
-        trainer = pl.Trainer(
-            accelerator="auto",
-            max_epochs=100,
-        )
-        print(dm)
-        trainer.fit(my_rul_estimator, dm)
-
-        trainer.test(my_rul_estimator, dm)
+    # read data
+    cmapss_fd1 = rul_datasets.CmapssReader(fd=1)
+    dm = rul_datasets.RulDataModule(
+        cmapss_fd1,
+        batch_size=30,
+    )
+    # instantiate model
+    cnn_lstm = Net(
+        dim_in=32,
+        dim_lstm=32,
+        kernel_size=32
+    )
+    # create trainer context
+    trainer = pl.Trainer(
+        accelerator='mps',
+        devices=1,
+        max_epochs=100
+    )
+    # fit & test
+    trainer.fit(cnn_lstm, dm)  # (2)!
+    trainer.test(cnn_lstm, dm)
