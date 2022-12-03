33166385e597b903d27937a3fdde8c2937429a28
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2022-12-04 00:48:00 +0300

    Working with lightning, only without mps.

--- models/models.py
+++ models/models.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 from pytorch_lightning import LightningModule
+from pytorch_lightning.utilities.types import STEP_OUTPUT
 
 
 class Shcherbakov(LightningModule):
@@ -155,24 +156,63 @@ class Test(LightningModule):
         return optimizer
 
 
-class Network(nn.Module):
+class Network(LightningModule):
     def __init__(self, in_channels, out_channels, kernel_size):
         super().__init__()
         # CNN feature extraction part
         self.conv_1 = nn.Conv1d(in_channels, out_channels, kernel_size)
         self.conv_2 = nn.Conv1d(out_channels, out_channels * 2, kernel_size - 2)
         self.relu = nn.ReLU()
-        self.lstm = nn.LSTM((64, 8), 50)
+        self.maxpool = nn.MaxPool1d(kernel_size=3)
+        self.num_layers = 4
+        self.lstm_1 = nn.LSTM(input_size=8, hidden_size=16, num_layers=self.num_layers)
+        self.lstm_2 = nn.LSTM(input_size=16, hidden_size=32, num_layers=self.num_layers)
+        self.dropout = nn.Dropout(p=0.2)
+        self.flatten = nn.Flatten()
         self.tanh = nn.Tanh()
+        self.regressor = nn.Linear(2048, 1)
 
     def forward(self, time_series):
+        # CNN feature extraction
         interim = self.relu(self.conv_1(time_series))
         features = self.relu(self.conv_2(interim))
+        features = self.maxpool(features)
 
-        h_0 = torch.zeros(features.size(0), features.size(0))
-        c_0 = torch.zeros(features.size(0), features.size(0))
+        # LSTM layers
+        h_0 = torch.zeros(self.num_layers, features.size(1), 16)
+        c_0 = torch.zeros(self.num_layers, features.size(1), 16)
         output, (h_n, c_n) = self.lstm_1(features, (h_0, c_0))
-        # h_n = F.dropout(F.tanh(h_n), p=0.2)
-        # c_n = F.dropout(F.tanh(c_n), p=0.2)
+        output = self.tanh(self.dropout(output))
 
-        return features
+        h_0 = torch.zeros(self.num_layers, output.size(1), output.size(2) * 2)
+        c_0 = torch.zeros(self.num_layers, output.size(1), output.size(2) * 2)
+        output, (h_n, c_n) = self.lstm_2(output, (h_0, c_0))
+        output = self.tanh(self.dropout(output))
+
+        # Regressor
+        output = self.flatten(output)
+        output = self.dropout(self.relu(self.regressor(output)))
+        return output
+
+    def training_step(self, batch) -> STEP_OUTPUT:
+        time_series, labels = batch
+        preds = self.forward(time_series)
+        loss = nn.functional.mse_loss(preds, labels)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        features, labels = batch
+        preds = self.forward(features)
+        loss = nn.functional.mse_loss(preds, labels)
+        self.log("val_loss", loss)
+
+    def test_step(self, batch, batch_idx, dataloader_idx=None):
+        features, labels = batch
+        preds = self.forward(features)
+        loss = nn.functional.mse_loss(preds, labels)
+        self.log(f"test_loss_fd{dataloader_idx + 1}", loss)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.1)
+        return optimizer
--- temp/test.py
+++ temp/test.py
@@ -1,11 +1,21 @@
 import rul_datasets
+import torch
+import warnings
 
-from models.models import *
+import pytorch_lightning as pl
+
+from models.models import Network
+pl.seed_everything(42)
+warnings.filterwarnings("ignore", ".*does not have many workers.*")
 
 if __name__ == "__main__":
-    reader = rul_datasets.CmapssReader(fd=1)
-    dev_features, dev_targets = reader.load_split("dev")
-    print(f'dev features: {dev_features[0].shape}')
-    x = torch.tensor(dev_features[0][:32], dtype=torch.float)
-    model = Network(in_channels=30, out_channels=32, kernel_size=5)
-    out = model.forward(x)
+    # read data
+    cmapss_fd1 = rul_datasets.CmapssReader(fd=1)
+    dm = rul_datasets.RulDataModule(cmapss_fd1, batch_size=32)
+    # create model
+    model = Network(in_channels=14, out_channels=32, kernel_size=5)
+    # create trainer context
+    trainer = pl.Trainer(devices=1, max_epochs=10)
+    # fit & test
+    trainer.fit(model, dm)
+    # trainer.test(model, dm)
