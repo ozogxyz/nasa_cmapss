5944f66ac1be378acc28fae56a3e84436209cc6c
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2022-12-05 15:49:45 +0300

    RMSE for regression works. Model simplified to feature_extractor and regressor.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     model = Network(in_channels=14, out_channels=32, kernel_size=5)
     # create trainer context
     logger = TensorBoardLogger("tb_logs", name="Shcherbakov")
-    trainer = pl.Trainer(accelerator="mps", devices=1, max_epochs=10, logger=logger)
+    trainer = pl.Trainer(devices=1, max_epochs=10, logger=logger)
     # fit & test
     trainer.fit(model, dm)
     trainer.test(model, dm)
--- temp/test.py
+++ temp/test.py
@@ -1,6 +1,7 @@
 import rul_datasets
 import warnings
 import pytorch_lightning as pl
+import torch
 from models.models import Network
 from pytorch_lightning.loggers import TensorBoardLogger
 
@@ -9,14 +10,10 @@ warnings.filterwarnings("ignore", ".*does not have many workers.*")
 
 if __name__ == "__main__":
     # read data
-    cmapss_fd1 = rul_datasets.CmapssReader(fd=1)
-    dm = rul_datasets.RulDataModule(cmapss_fd1, batch_size=32)
-    # create model
-    model = Network(in_channels=14, out_channels=32, kernel_size=5)
-    # create trainer context
-    logger = TensorBoardLogger("tb_logs", name="Shcherbakov")
-    trainer = pl.Trainer(accelerator="mps", devices=1, max_epochs=10, logger=logger)
-    # fit & test
-    trainer.fit(model, dm)
-    trainer.test(model, dm)
+    model = Network(in_channels=30, out_channels=32, kernel_size=5)
+    dev_features, dev_targets = rul_datasets.CmapssReader(fd=1).load_split("dev")
+    time_series = torch.tensor(dev_features[0], dtype=torch.float)
+    preds = model.forward(time_series).detach()
+    targets = torch.tensor(dev_targets[0])
+    print(preds)
 
