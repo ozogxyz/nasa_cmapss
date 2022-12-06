a59d209985b9474db4df7bcaec0344c5a616bf56
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2022-12-06 17:38:01 +0300

    Dumm data reader for debugging added.

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import rul_datasets
 import warnings
 import pytorch_lightning as pl
-from models.models import Network, Ozoglu
+from models.models import Shcherbakov
 from pytorch_lightning.loggers import TensorBoardLogger
 
 pl.seed_everything(42)
 warnings.filterwarnings("ignore", ".*does not have many workers.*")
+
 
 if __name__ == "__main__":
     # read data
     cmapss_fd1 = rul_datasets.CmapssReader(fd=1)
     dm = rul_datasets.RulDataModule(cmapss_fd1, batch_size=32)
     # create model
-    # model = Network(in_channels=14, out_channels=32, kernel_size=5)
-    model = Ozoglu(in_channels=14, out_channels=32, kernel_size=5)
+    model = Shcherbakov()
     # create trainer context
     logger = TensorBoardLogger("tb_logs", name="Shcherbakov")
-    trainer = pl.Trainer(accelerator="auto", devices=1, max_epochs=10, logger=logger)
+    trainer = pl.Trainer(accelerator="auto", devices=1, max_epochs=100, logger=logger)
     # fit & test
     trainer.fit(model, dm)
     trainer.test(model, dm)
-
--- models/models.py
+++ models/models.py
@@ -4,130 +4,86 @@ from pytorch_lightning import LightningModule
 from pytorch_lightning.utilities.types import STEP_OUTPUT
 
 
-class Network(LightningModule):
-    def __init__(self, in_channels, out_channels, kernel_size):
+class RMSELoss(nn.Module):
+    def __init__(self, eps=1e-6):
         super().__init__()
-        # CNN feature extraction part
-        self.conv_1 = nn.Conv1d(in_channels, out_channels, kernel_size)
-        self.conv_2 = nn.Conv1d(out_channels, out_channels * 2, kernel_size - 2)
-        self.relu = nn.ReLU()
-        self.flatten = nn.Flatten(start_dim=1)
-        self.maxpool = nn.MaxPool1d(kernel_size=3)
-        self.num_layers = 50
-        self.lstm_1 = nn.LSTM(input_size=512, hidden_size=512, num_layers=self.num_layers)
-        self.lstm_2 = nn.LSTM(input_size=512, hidden_size=512, num_layers=self.num_layers)
-        self.dropout = nn.Dropout(p=0.2)
-        self.tanh = nn.Tanh()
-        self.regressor = nn.Linear(512, 1)
-
-    def forward(self, time_series):
-        # CNN feature extraction
-        interim = self.relu(self.conv_1(time_series))
-        features = self.relu(self.conv_2(interim))
-        features = self.flatten(features)
-        features = self.maxpool(features)
-
-        # LSTM layers
-        h_0 = torch.zeros(self.num_layers, features.size(1)).to("mps")
-        c_0 = torch.zeros(self.num_layers, features.size(1)).to("mps")
-        output, (h_n, c_n) = self.lstm_1(features, (h_0, c_0))
-        output = self.tanh(self.dropout(output))
-
-        output, (h_n, c_n) = self.lstm_2(output, (h_n, c_n))
-        output = self.tanh(self.dropout(output))
-
-        # Regressor
-        output = self.relu(self.regressor(output))
-        return output
-
-    def training_step(self, batch) -> STEP_OUTPUT:
-        time_series, labels = batch
-        preds = self.forward(time_series)
-        loss = torch.sqrt(torch.nn.functional.mse_loss(preds.flatten(), labels))
-        self.log("train_loss", loss)
+        self.mse = nn.MSELoss()
+        self.eps = eps
+        
+    def forward(self, predictions, labels):
+        loss = torch.sqrt(self.mse(predictions.flatten(), labels) + self.eps)
         return loss
 
-    def validation_step(self, batch, batch_idx):
-        features, labels = batch
-        preds = self.forward(features)
-        loss = torch.sqrt(torch.nn.functional.mse_loss(preds.flatten(), labels))
-        self.log("val_loss", loss)
 
-    def test_step(self, batch, batch_idx, dataloader_idx=0):
-        features, labels = batch
-        preds = self.forward(features)
-        loss = torch.sqrt(torch.nn.functional.mse_loss(preds.flatten(), labels))
-        self.log(f"test_loss_fd{dataloader_idx + 1}", loss)
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.1)
-        return optimizer
-
-
-class Ozoglu(LightningModule):
-    def __init__(self, in_channels, out_channels, kernel_size):
+class Shcherbakov(LightningModule):
+    def __init__(self, in_channels=14, out_channels=32, kernel_size=5, num_layers=2, hidden_size=16):
         super().__init__()
-        # CNN feature extraction part
-        self.feature_extractor = nn.Sequential()
-        self.feature_extractor.add_module(
-            'Conv1d_1', nn.Conv1d(in_channels, out_channels, kernel_size)
-        )
-        self.feature_extractor.add_module('ReLU_1', nn.ReLU())
-        self.feature_extractor.add_module(
-            'Conv1d_2', nn.Conv1d(out_channels, out_channels * 2, kernel_size - 2)
-        )
-        self.feature_extractor.add_module('ReLU_2', nn.ReLU())
-        self.feature_extractor.add_module('Flatten', nn.Flatten(start_dim=1))
-        self.feature_extractor.add_module(
-            'MaxPool1d', nn.MaxPool1d(kernel_size=3)
-        )
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
 
-        self.lstm_1 = nn.LSTM(input_size=512, hidden_size=512, num_layers=50)
-        self.lstm_2 = nn.LSTM(input_size=512, hidden_size=512, num_layers=50)
-        self.dropout = nn.Dropout(p=0.2)
-        self.num_layers = 50
-        self.tanh = nn.Tanh()
+        # CNN feature extraction part
+        self.conv_1 = nn.Sequential(
+            nn.Conv1d(in_channels, out_channels, kernel_size),
+            nn.ReLU(),
+        )
+        self.conv_2 = nn.Sequential(
+            nn.Conv1d(out_channels, out_channels * 2, kernel_size - 2),
+            nn.ReLU(),
+        )
+        self.maxpool = nn.MaxPool1d(kernel_size=3)
+
+        # LSTM Part
+        self.lstm = nn.LSTM(input_size=512, hidden_size=self.hidden_size, num_layers=self.num_layers, dropout=0.2)
 
         # Regressor part
-        self.regressor = nn.Sequential()
-        self.regressor.add_module('Linear_1', nn.Linear(512, 1))
-        self.regressor.add_module('ReLU_2', nn.ReLU())
-
-        # Loss function
-        self.loss = nn.MSELoss()
+        self.regressor = nn.Sequential(
+            # nn.Linear(512, 256),
+            # nn.ReLU(),
+            # nn.Linear(256, 128),
+            # nn.ReLU(),
+            # nn.Linear(128, 64),
+            # nn.ReLU(),
+            nn.Linear(16, 1),
+        )
 
     def forward(self, time_series):
         # CNN feature extraction
-        features = self.feature_extractor(time_series)
-
+        features = self.conv_1(time_series)
+        features = self.conv_2(features)
+        features = self.maxpool(features)
+        features = torch.flatten(features, start_dim=1)
         # LSTM layers
-        h_0 = torch.zeros(self.num_layers, features.size(1)).to("mps")
-        c_0 = torch.zeros(self.num_layers, features.size(1)).to("mps")
-        output, (h_n, c_n) = self.lstm_1(features, (h_0, c_0))
-        output = self.tanh(self.dropout(output))
-        output, (h_n, c_n) = self.lstm_2(output, (h_n, c_n))
-        output = self.tanh(self.dropout(output))
-        preds = self.regressor(features)
-        return preds
+        # h_0 = torch.zeros(self.num_layers, self.hidden_size).to("mps")
+        # c_0 = torch.zeros(self.num_layers, self.hidden_size).to("mps")
+        h_0 = torch.zeros(self.num_layers, self.hidden_size)
+        c_0 = torch.zeros(self.num_layers, self.hidden_size)
+        output, (h_n, c_n) = self.lstm(features, (h_0, c_0))
+        output = torch.tanh(output)
+        # Regressor
+        output = self.regressor(output)
+        return output
 
     def training_step(self, batch):
         time_series, labels = batch
-        preds = self.forward(time_series)
-        loss = torch.sqrt(self.loss(preds.flatten(), labels))
+        predictions = self.forward(time_series)
+        loss = RMSELoss().forward(predictions=predictions, labels=labels)
         self.log("train_loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         features, labels = batch
-        preds = self.forward(features)
-        loss = torch.sqrt(self.loss(preds.flatten(), labels))
+        predictions = self.forward(features)
+        loss = RMSELoss().forward(predictions=predictions, labels=labels)
         self.log("val_loss", loss)
 
     def test_step(self, batch, batch_idx, dataloader_idx=1):
         features, labels = batch
-        preds = self.forward(features)
-        loss = torch.sqrt(self.loss(preds.flatten(), labels))
-        self.log(f'RMSE for dataset FD00{dataloader_idx}.txt:\t {loss:.3f}')
+        predictions = self.forward(features)
+        loss = RMSELoss().forward(predictions=predictions, labels=labels)
+        self.log(f'RMSE for dataset FD00{dataloader_idx}.txt', loss)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
