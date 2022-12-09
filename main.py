866042bf1a220bb78b949f8e38db886fdf56ae43
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2022-12-09 16:28:28 +0300

    Flatten added to main.py

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ if __name__ == "__main__":
         description="Runs the trainer for selected models.",
         epilog="See individual options with --help."
     )
-    parser.add_argument('-f', '--filename', help='[Data subset number of NASA CMAPSS Turbofan Engine data. '
-                                                 'Default is subset 1.]', default=1, type=int)
+    parser.add_argument('-fd', '--filename', help='[Data subset number of NASA CMAPSS Turbofan Engine data. '
+                                                  'Default is subset 1.]', default=1, type=int)
     parser.add_argument('-b', '--batch_size', help='[Batch size to send to the trainer. '
                                                    'Default is 200.]', default=200, type=int)
     parser.add_argument('-e', '--epochs', help='[Maximum number of iterations for the trainer. '
                                                'Default is 10.]', default=10, type=int)
+    parser.add_argument('--flatten', help='[Flatten the output of Conv1d layer output.]', default=False, type=bool)
 
     args = vars(parser.parse_args())
 
@@ -32,7 +33,7 @@ if __name__ == "__main__":
     cmapss_fd1 = rul_datasets.CmapssReader(fd)
     dm = rul_datasets.RulDataModule(cmapss_fd1, batch_size=batch_size)
     # create model
-    model = Cnn1dLSTM()
+    model = Cnn1dLSTM(flatten=True)
     # create trainer context
     logger = TensorBoardLogger("tb_logs", name="Cnn1dLSTM")
     trainer = pl.Trainer(accelerator="auto", devices=1, max_epochs=max_epochs, logger=logger)
