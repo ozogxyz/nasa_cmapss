b59f9f0e0373b36ef3a2a51db3a27da8e491a759
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2022-12-11 11:57:58 +0300

    param passing w/ YAML

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import argparse
+import os
 import rul_datasets
+import yaml
 import warnings
 import pytorch_lightning as pl
 from models.cnn1d_lstm import Cnn1dLSTM
@@ -9,46 +10,50 @@ pl.seed_everything(42)
 warnings.filterwarnings("ignore", ".*does not have many workers.*")
 
 if __name__ == "__main__":
-    # read command line arguments
-    parser = argparse.ArgumentParser(
-        prog="Trainer",
-        description="Runs the trainer for selected models.",
-        epilog="See individual options with --help."
-    )
-    parser.add_argument('-fd', '--filename', help='[Data subset number of NASA CMAPSS Turbofan Engine data. '
-                                                  'Default is subset 1.]', default=1, type=int)
-    parser.add_argument('-b', '--batch_size', help='[Batch size to send to the trainer. '
-                                                   'Default is 200.]', default=200, type=int)
-    parser.add_argument('-e', '--epochs', help='[Maximum number of iterations for the trainer. '
-                                               'Default is 10.]', default=10, type=int)
-    parser.add_argument('--flatten', help='[Flatten the output of Conv1d layer output.]', default=False, type=bool)
+    # read hyperparameters
+    with open('params.yaml', 'r') as file:
+        params = yaml.safe_load(file)
 
-    args = vars(parser.parse_args())
-
-    fd = args['filename']
-    batch_size = args['batch_size']
-    max_epochs = args['epochs']
+    batch_size = params.get('batch_size')
+    fd = params.get('filename')
+    max_epochs = params.get('epochs')
+    in_channels = params.get('in_channels')
+    out_channels = params.get('out_channels')
+    kernel_size = params.get('kernel_size')
+    maxpool_kernel = params.get('maxpool_kernel')
+    num_classes = params.get('num_classes')
+    hidden_size = params.get('hidden_size')
+    num_layers = params.get('num_layers')
+    maxpool_stride = params.get('maxpool_stride')
+    window_size = params.get('window_size')
+    flatten = params.get('flatten')
+    lr = params.get('lr')
 
     # read data
     cmapss_fd1 = rul_datasets.CmapssReader(fd)
     dm = rul_datasets.RulDataModule(cmapss_fd1, batch_size=batch_size)
     # create model
-    model = Cnn1dLSTM(        
-        batch_size = batch_size,
-        in_channels = 14,
-        out_channels = 64,
-        kernel_size = 5,
-        maxpool_kernel = 3,
-        num_classes = 1,
-        input_size = 14,
-        hidden_size = 96,
-        num_layers = 2,
-        maxpool_stride = 2,
-        window_size = 30,
-        flatten = False,)
+    model = Cnn1dLSTM(
+                batch_size = batch_size,
+                in_channels = in_channels,
+                out_channels = out_channels,
+                kernel_size = kernel_size,
+                maxpool_kernel = maxpool_kernel,
+                num_classes = num_classes,
+                hidden_size = hidden_size,
+                num_layers = num_layers,
+                maxpool_stride = maxpool_stride,
+                window_size = window_size,
+                flatten = flatten,
+                lr = lr,
+    )
+
     # create trainer context
     logger = TensorBoardLogger("tb_logs", name="Cnn1dLSTM")
     trainer = pl.Trainer(accelerator='auto',max_epochs=max_epochs, logger=logger)
+
+    os.system("bat params.yaml")
+
     # fit & test
     trainer.fit(model, dm)
     trainer.test(model, dm)
