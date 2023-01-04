38e05ec5a0bdef65f0a443901c4bfafa9cbbd7db
Orkun Ozoglu <50372354+ozogxyz@users.noreply.github.com>
2023-01-05 00:39:03 +0300

    hydra defaults config

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import hydra
 import hydra_zen
 from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import RichModelSummary, RichProgressBar
+from pytorch_lightning.callbacks import RichModelSummary, RichProgressBar, EarlyStopping, Timer, ModelCheckpoint
 from rul_datasets.core import RulDataModule
 from rul_datasets.reader.cmapss import CmapssReader
 
@@ -25,16 +25,37 @@ def main(cfg: DictConfig) -> None:
     # train model
     model = Network(**cfg.model)
 
+    # callbacks
+    early_stopping = EarlyStopping(
+        check_on_train_epoch_end=True,
+        monitor="val_loss",
+        patience=cfg.training.patience,
+        strict=False,
+        verbose=True,
+        min_delta=cfg.training.min_delta,
+        mode='min'
+    )
+    ckpt = ModelCheckpoint(cfg.training.save_dir)
+    summary = RichModelSummary()
+    progress = RichProgressBar()
+    timer = Timer()
+
+    callbacks = [ckpt, early_stopping, progress, timer, summary]
+
     trainer = Trainer(
         accelerator='mps', 
         max_epochs=cfg.training.max_epochs,
-        callbacks=[RichModelSummary(), RichProgressBar()],
+        log_every_n_steps=cfg.training.log_every_n_steps,
+        callbacks=callbacks,
+        gradient_clip_val=cfg.training.gradient_clip_val,
+        gradient_clip_algorithm=cfg.training.gradient_clip_algorithm,
+        max_steps=cfg.training.max_steps,
+        default_root_dir=cfg.training.lightning_log_dir
     )
     trainer.fit(model, dm)
 
-    # evaluate
-    evaluator = hydra_zen.Evaluator(model, dm)
-    evaluator.evaluate()
+    # test
+    trainer.test(model, dm, ckpt_path="best")
 
 
 if __name__ == "__main__":
